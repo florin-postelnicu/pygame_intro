e2517e70c59ac7bba5e69d627825a08d1e90348c
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-02-15 14:40:48 -0700

    Add files via upload

--- Intor_to_pygame/secret_word.py
+++ Intor_to_pygame/secret_word.py
@@ -8,34 +8,85 @@ than it should be converted to a string
 '''
 import pygame
 import os
-import datetime
 from hangman_list import text_w
 import random
 
-# Define some colors
 
 
-def draw_text(text):
+
+def draw_text(text, font_size):
     # Select the font to use, size, bold, italics
-    font = pygame.font.SysFont('Times New Roman', 50, True, False)
+    font = pygame.font.SysFont('Times New Roman', font_size, True, False)
     # Render the text. "True" means anti-aliased text.
     # Black is the color. This creates an image of the
     # letters, but does not put it on the screen
     text_new = font.render(text, True, RED)
     return text_new
-
-def blinking():
-
-    if event.type == pygame.MOUSEMOTION:
-        pos = pygame.mouse.get_pos()
-        if pos[0] >= 50 and pos[0] <= 700 and pos[1] >= 550 and pos[1] <= 650:
-            columny = (pos[0] - LETTER_BOX[0]) // WIDTH_LETTER_BOX
-            rowx = (pos[1] - LETTER_BOX[1]) // HEIGHT_LETTER_BOX
-            # imagebs = screen.blit(YELLOW_SQUARE, (50 + 50 * columny, 550 + 50 * rowx))
-            imagebs = pygame.draw.rect(screen, (255,25,255, 70), [50 + 50 * columny, 550 + 50 * rowx, 50,50], 8)
-            return imagebs
+# Classes
 
 
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, image_file, location):
+        pygame.sprite.Sprite.__init__(self)  # Call Sprite Initializer
+        self.image = pygame.image.load(image_file)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
+
+class Gallows(pygame.sprite.Sprite):
+    def __init__(self, image_file,  location):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(image_file).convert()
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
+
+
+class Grid(pygame.sprite.Sprite):
+    global LETTER_BOX, WIDTH_LETTER_BOX, HEIGHT_LETTER_BOX, LETTERS
+    LETTER_BOX = (50, 550)
+    WIDTH_LETTER_BOX = 50
+    HEIGHT_LETTER_BOX = 50
+    LETTERS = [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'],
+               ['N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']]
+    def __init__(self, ):
+        pygame.sprite.Sprite.__init__(self)
+        self.table = []
+        for row in range(2):
+            # Add an empty array that will hold each cell
+            # in this row
+            self.table.append([])
+            for column in range(13):
+                self.table[row].append(LETTERS[row][column])  # Append a cell
+    # @staticmethod
+    def blinking (self):
+        if event.type == pygame.MOUSEMOTION:
+            pos = pygame.mouse.get_pos()
+            if pos[0] >= 50 and pos[0] <= 700 and pos[1] >= 550 and pos[1] <= 650:
+                columny = (pos[0] - LETTER_BOX[0]) // WIDTH_LETTER_BOX
+                rowx = (pos[1] - LETTER_BOX[1]) // HEIGHT_LETTER_BOX
+                pygame.draw.rect(screen, (255,25,255, 70), [50 + 50 * columny, 550 + 50 * rowx, 50,50], 8)
+
+
+    def drawgrid(self):
+        for row in range(2):
+            for column in range(13):
+                screen.blit(draw_text(self.table[row][column], 50), [50 + 50 * column, 550 + 50 * row])
+
+    def update(self):
+        self.blinking()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # User clicks the mouse. Get the position
+            pos = pygame.mouse.get_pos()
+            if pos[0] >= 50 and pos[0] <= 700 and pos[1] >= 550 and pos[1] <= 650:
+                # Change the x/y screen coordinates to grid coordinates
+                column = (pos[0] - LETTER_BOX[0]) // WIDTH_LETTER_BOX
+                row = (pos[1] - LETTER_BOX[1]) // HEIGHT_LETTER_BOX
+                # # Set that location to one
+                # grid[row][column] = 1
+                print("Click ", pos, "Grid coordinates: ", row, column, 'letter is ', self.table[row][column])
+
+
+# Define some colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
@@ -44,7 +95,7 @@ BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
 
-index = random.randint(0, len(text_w))
+index = random.randint(0, len(text_w)-1)
 word = text_w[index]
 pygame.init()
 
@@ -54,51 +105,29 @@ WIDTH = 1200
 HEIGHT = 700
 size = (WIDTH, HEIGHT)
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption('Letter Box')
+pygame.display.set_caption('Secret Word')
 
-YELLOW_SQUARE = pygame.image.load('yellow_square.png').convert()
-LETTER_BOX = (50, 550)
-WIDTH_LETTER_BOX = 50
-HEIGHT_LETTER_BOX = 50
-
-LETTERS = [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'],
-           ['N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']]
-# Create a 2 dimensional array. A two dimensional
-# array is simply a list of lists.
-grid = []
-for row in range(2):
-    # Add an empty array that will hold each cell
-    # in this row
-    grid.append([])
-    for column in range(13):
-        grid[row].append(LETTERS[row][column])  # Append a cell
-# Loop until the user clicks the close button
 done = False
 # Use how fast the screen updates
 clock = pygame.time.Clock()
+allsprites = pygame.sprite.Group()
+grid1 = Grid()
+allsprites.add(grid1)
 
-
+BckG = Background('bkg.jpg', [0,0])
 # --------------Main Program Loop -------------
 while not done:
-    global column_m, row_m
+
     # ---Main event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            # User clicks the mouse. Get the position
-            pos = pygame.mouse.get_pos()
-            if pos[0] >= 50 and pos[0] <= 700 and pos[1] >= 550 and pos[1] <= 650:
-                # Change the x/y screen coordinates to grid coordinates
-                column =( pos[0] - LETTER_BOX[0])// WIDTH_LETTER_BOX
-                row = (pos[1] -LETTER_BOX[1])// HEIGHT_LETTER_BOX
-                # # Set that location to one
-                # grid[row][column] = 1
-                print("Click ", pos, "Grid coordinates: ", row, column, 'letter is ', grid[row][column])
+        else :
+             Grid.update(grid1)
+
 
     # Game Logic should go here
-    # something = datetime.datetime.now()
-    # something.strftime("%Y-%m-%d  %H:%M:%S")
+
 
     # Screen clearing goes here
 
@@ -106,32 +135,43 @@ while not done:
     #  above this , or they will be erased with this command
 
     # If you want a background image, replace this clear with
-    # blit' ing the background image.
-    screen.fill(WHITE)
+    
+    screen.blit(BckG.image, BckG.rect)
 
     # --------Drawing code should go here
     # rect1 is for the secret word
 
-    rect1 = pygame.draw.rect(screen, YELLOW, [200, 200, 50*len(word), 100], 5)
+    rect1 = pygame.draw.rect(screen, YELLOW, [50, 400, 50*(len(word)-1), 100], 3)
     # rect 2 is LETTER_BOX
-    rect2 = pygame.draw.rect(screen, RED, [LETTER_BOX[0],LETTER_BOX[1], 13*WIDTH_LETTER_BOX, 2*HEIGHT_LETTER_BOX], 1)
+    rect2 = pygame.draw.rect(screen, YELLOW, [LETTER_BOX[0],LETTER_BOX[1], 13*WIDTH_LETTER_BOX, 2*HEIGHT_LETTER_BOX], 3)
 
-    # ---------Update thje screen with what we have drawn
-    # ---------Put the image of the text on the screen at 250 x250
+    # ---------Update the screen with what we have drawn
+    # ---------Put the image of the text on the screen at 220 x220
 
-    screen.blit(draw_text(str(word.upper())), [220, 220])
+    screen.blit(draw_text(str(word.upper()), 50), [60, 420])
 
     # Draw each letter of the alphabet on the corresponding location
-    blinking()
-    for row in range(2):
-        for column in range(13):
-            # blinking()
-            screen.blit(draw_text(grid[row][column]), [50 + 50 * column, 550 + 50 * row])
+    Grid.blinking(grid1)
+    Grid.drawgrid(grid1)
+    # for row in range(2):
+    #     for column in range(13):
+    #         # blinking()
+    #         screen.blit(draw_text(grid[row][column], 50), [50 + 50 * column, 550 + 50 * row])
+
+    allsprites.update()
+    # screen.fill(BLACK)
+    # ------Limit 60 frames per second
+    clock.tick(60)
+    pygame.event.pump()
+    # Go ahead and update the screen with what we've drawn.
+    # if the classes have attributes images , then allsprites.draw()
+    # allsprites.draw(screen)
+
+
 
     pygame.display.flip()
 
-    # ------Limit 60 frames per second
-    clock.tick(60)
 
-# Close the window and quit.
+
+    # Close the window and quit.
 pygame.quit()
