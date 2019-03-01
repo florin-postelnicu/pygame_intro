66641a4f99cbb4dbfe9762d68b3424a09074a655
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-02-28 19:55:14 -0700

    Update anim_01.py

--- Intor_to_pygame/anim_01.py
+++ Intor_to_pygame/anim_01.py
@@ -1,36 +1,37 @@
 
 '''
 The size of a sprite in this example is 64x64 pixels.
-The sprite-sheet rob2.png is a 6 rows  by 9 columns.The numbers of the lines are given like in the program anim_01.py
+The sprite-sheet rob2.png is a 6 rows  by 9 columns.
 
 Step1 Load the whole sprite-sheet
-59   page = os.path.join('images', 'rob2.png')
-60   sheet = pygame.image.load(page).convert_alpha()
+   page = os.path.join('images', 'rob2.png')
+   sheet = pygame.image.load(page).convert_alpha()
 
 Step 2 Break the sprite-sheet in 54 independent images , reading one-by-one,
-and append each such independent image(frame) to the list of frames.
-62   ind_images = []
-63   for row in range(9):
-64        for column in range(6):
-65            single_image = sheet.subsurface((64*row, 64*column, 64, 64))
-66            ind_images.append(single_image)
+and append each such independent image(frame) to the list of frames(ind_images).
+   ind_images = []
+   for row in range(9):
+        for column in range(6):
+            single_image = sheet.subsurface((64*row, 64*column, 64, 64))
+            ind_images.append(single_image)
 
 Step 3 Set up the ticking clock for displaying the frames in the list of frames
-67    nextFrame = pygame.time.get_ticks()
-68    frame = 0
+    nextFrame = pygame.time.get_ticks()
+    frame = 0
 
 Step 4 Inside the main loop blit in order every one of the frames in your list,
 by allowing a time of 50 miliseconds between every other frame.
-92     now = pygame.time.get_ticks()
-93   if  now - nextFrame> 50:
-94       frame = (frame +1) %len(ind_images)
-95       screen.blit(ind_images[frame], ( aero_x, aero_y))
-96       nextFrame = now
+     now = pygame.time.get_ticks()
+     if  now - nextFrame> 50:
+          frame = (frame +1) %len(ind_images)
+          screen.blit(ind_images[frame], ( aero_x, aero_y))
+          nextFrame = now
 
 
 Note !
-88    screen.fill(BLACK) , or (WHITE) should be placed before blitting the background
-its purpose is to hide the previous frame when the next one is blit.
+      screen.fill(BLACK) 
+or (WHITE) should be placed before blitting the background.
+Its purpose is to hide the previous frame when the next one is blit.
 '''
 
 
@@ -64,16 +65,21 @@ clock = pygame.time.Clock()
 bg = os.path.join('images', 'bkg.jpg')
 BckG = pygame.image.load(bg)
 # The next two lines are to load the sprite sheet
+# Step 1 
 page = os.path.join('images', 'rob2.png')
 sheet = pygame.image.load(page).convert_alpha()
-
+# End of Step1
+# Step 2
 ind_images = []
 for row in range(9):
     for column in range(6):
         single_image = sheet.subsurface((64 * row, 64 * column, 64, 64))
         ind_images.append(single_image)
+# End of Step 3
+# Step 3
 nextFrame = pygame.time.get_ticks()
 frame = 0
+# End of Step 3
 aero_x = 200
 aero_y = 200
 
@@ -93,12 +99,13 @@ while not done:
 
 
     # ---------Update thje screen with what we have drawn
-
+    # Step 4
     now = pygame.time.get_ticks()
     if now - nextFrame > 50:
         frame = (frame + 1) % len(ind_images)
         screen.blit(ind_images[frame], (aero_x, aero_y))
         nextFrame = now
+    # End of Step 4
     pygame.display.flip()
 
     # ------Limit 60 frames per second
